dcd9c9997dcbb915689a6e866e456188c2319a25
daniil <daniil>
2021-09-11 11:56:51 +0300

    js

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,17 @@
 console.log('Hello, world!');
+let btnc = document.querySelector('.banner__btn_colored');
+let formc = document.querySelectorAll('div.modal-overlay, div.modal, div.modal-form');
+btnc.addEventListener('click', function(e){
+    formc.classList.add('active');
+});
 
-var a = 10;
-console.log(a);
+let btnt = document.querySelector('.banner__btn_transparent');
+let formt = document.querySelector('.modal-overlay');
+btnt.addEventListener('click', function(e){
+    formt.classList.add('active');
+});
+
+// document.getElement('banner__btn_colored').addEventListener('click', () => {
+//     document.querySelector('modal-form').classList.toggle('is-active');
+  
+//   });
